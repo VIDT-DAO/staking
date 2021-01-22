41c60bb9a6d2e92fdbdcf9b3004d1384e77c8993
Arnold Daniels <arnold@jasny.net>
2021-01-22 03:44:44 +0100

    Implemented caps for Deposit contract. Setting a cap for a token limits the amount that may be deposited by a user.

--- contracts/Deposits.sol
+++ contracts/Deposits.sol
@@ -13,6 +13,14 @@ contract Deposits is Ownable {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
+    uint256 MAX_INT = uint256(-1);
+
+    struct capInfo {
+        IERC20 referenceToken;
+        uint256 referenceAmount;
+        uint256 cappedAmount;
+    }
+
     // Info of each deposit.
     struct DepositInfo {
         IERC20 token;   // Address of token contract.
@@ -20,12 +28,28 @@ contract Deposits is Ownable {
         uint256 block;  // Block that the amount was deposited.
     }
 
+    mapping (IERC20 => capInfo) public caps;
+
     // List of deposits of each user.
     mapping (address => DepositInfo[]) public deposits;
+    // Total deposit per token.
     mapping (IERC20 => uint256) public totals;
 
     event Deposit(address indexed user, IERC20 indexed token, uint256 amount);
     event Withdraw(address indexed user, IERC20 indexed token, uint256 amount);
+
+    // Set the maximum amount of tokens that can be deposited foreach reference token.
+    function cap(IERC20 _referenceToken, uint256 _referenceAmount, IERC20 _cappedToken, uint256 _cappedAmount) public onlyOwner {
+        caps[_cappedToken] = capInfo({
+            referenceToken: _referenceToken,
+            referenceAmount: _referenceAmount,
+            cappedAmount: _cappedAmount
+        });
+    }
+
+    function uncap(IERC20 _cappedToken) public onlyOwner {
+        delete caps[_cappedToken];
+    }
 
     // View function to see deposited tokens.
     function total(IERC20 _token) external view returns (uint256) {
@@ -33,7 +57,7 @@ contract Deposits is Ownable {
     }
 
     // View function to see deposited tokens for a user.
-    function deposited(IERC20 _token, address _user) external view returns (uint256) {
+    function deposited(IERC20 _token, address _user) public view returns (uint256) {
         DepositInfo[] storage userDeposits = deposits[_user];
         uint256 length = userDeposits.length;
         uint256 amount = 0;
@@ -47,8 +71,30 @@ contract Deposits is Ownable {
         return amount;
     }
 
-    // Deposit pool tokens to contract for ERC20 allocation.
+    // The maximum amount of capped tokens the user is still allowed to deposit.
+    function maxDeposit(IERC20 _token, address _user) public view returns (uint256) {
+        capInfo storage info = caps[_token];
+
+        if (info.referenceAmount == 0) {
+            return MAX_INT;
+        }
+
+        uint256 depositedReference = deposited(info.referenceToken, _user);
+        uint256 depositedCapped = deposited(_token, _user);
+
+        uint256 maxAmount = depositedReference.mul(info.cappedAmount).div(info.referenceAmount);
+
+        if (depositedCapped >= maxAmount) {
+            return 0;
+        }
+
+        return maxAmount.sub(depositedCapped);
+    }
+
+    // Deposit tokens to the contract.
     function deposit(IERC20 _token, uint256 _amount) public {
+        require(_amount <= maxDeposit(_token, msg.sender), "Not allowed to deposit specified amount of capped token");
+
         _token.safeTransferFrom(address(msg.sender), address(this), _amount);
 
         deposits[msg.sender].push(DepositInfo({
@@ -62,7 +108,8 @@ contract Deposits is Ownable {
         emit Deposit(msg.sender, _token, _amount);
     }
 
-    // Withdraw without caring about rewards.
+    // Withdraw all tokens from the contract.
+    // Withdrawing direct from the Deposit contract, means you won't receive any rewards.
     function withdrawWithoutReward() public {
         DepositInfo[] storage userDeposits = deposits[msg.sender];
         uint256 length = userDeposits.length;
--- test/Deposits.test.js
+++ test/Deposits.test.js
@@ -31,7 +31,7 @@ contract('Deposits', ([owner, alice, bob, carl]) => {
         this.token2 = await ERC20.new("Token 2", "T2", 0, 0);
 
         await Promise.all([
-            this.token2.mint(alice, 1000),
+            this.token2.mint(alice, 1500),
             this.token2.mint(bob, 800),
         ]);
 
@@ -41,7 +41,7 @@ contract('Deposits', ([owner, alice, bob, carl]) => {
             this.token2.balanceOf(carl),
         ]);
 
-        assert.equal(1000, balanceAlice);
+        assert.equal(1500, balanceAlice);
         assert.equal(800, balanceBob);
         assert.equal(0, balanceCarl);
     });
@@ -52,12 +52,16 @@ contract('Deposits', ([owner, alice, bob, carl]) => {
                 this.token1.approve(this.deposits.address, 3000, { from: alice }),
                 this.token2.approve(this.deposits.address, 1000, { from: alice }),
                 this.token1.approve(this.deposits.address, 500, { from: bob }),
+                this.token2.approve(this.deposits.address, 100, { from: bob }),
+                this.token1.approve(this.deposits.address, 1000, { from: carl })
             ]);
 
             await Promise.all([
                 this.deposits.deposit(this.token1.address, 1500, {from: alice}),
                 this.deposits.deposit(this.token2.address, 1000, {from: alice}),
-                this.deposits.deposit(this.token1.address, 500, {from: bob})
+                this.deposits.deposit(this.token1.address, 500, {from: bob}),
+                this.deposits.deposit(this.token2.address, 100, {from: bob}),
+                this.deposits.deposit(this.token1.address, 1000, {from: carl})
             ]);
         });
 
@@ -69,7 +73,7 @@ contract('Deposits', ([owner, alice, bob, carl]) => {
 
             const balanceToken2 = await this.token2.balanceOf(alice);
             const depositToken2 = await this.deposits.deposited(this.token2.address, alice);
-            assert.equal(0, balanceToken2);
+            assert.equal(500, balanceToken2);
             assert.equal(1000, depositToken2);
         });
 
@@ -81,16 +85,86 @@ contract('Deposits', ([owner, alice, bob, carl]) => {
 
             const balanceToken2 = await this.token2.balanceOf(bob);
             const depositToken2 = await this.deposits.deposited(this.token2.address, bob);
-            assert.equal(800, balanceToken2);
+            assert.equal(700, balanceToken2);
+            assert.equal(100, depositToken2);
+        });
+
+
+        it('shows the deposits of carl', async () => {
+            const balanceToken1 = await this.token1.balanceOf(carl);
+            const depositToken1 = await this.deposits.deposited(this.token1.address, carl);
+            assert.equal(1000, balanceToken1);
+            assert.equal(1000, depositToken1);
+
+            const balanceToken2 = await this.token2.balanceOf(carl);
+            const depositToken2 = await this.deposits.deposited(this.token2.address, carl);
+            assert.equal(0, balanceToken2);
             assert.equal(0, depositToken2);
         });
 
         it('shows the total deposit', async () => {
             const totalToken1 = await this.deposits.total(this.token1.address);
-            assert.equal(2000, totalToken1);
+            assert.equal(3000, totalToken1);
 
             const totalToken2 = await this.deposits.total(this.token2.address);
-            assert.equal(1000, totalToken2);
+            assert.equal(1100, totalToken2);
         });
-    })
-});
+    });
+
+    describe('has a capped token', () => {
+        before(async () => {
+            await this.deposits.cap(this.token1.address, 20, this.token2.address, 10);
+        });
+
+        it('shows alice can\'t deposit token 2', async () => {
+            const max = await this.deposits.maxDeposit(this.token2.address, alice);
+            assert.equal(0, max);
+        });
+
+        it('shows bob can deposit some token 2', async () => {
+            const max = await this.deposits.maxDeposit(this.token2.address, bob);
+            assert.equal(150, max);
+        });
+
+        it('shows carl can deposit max token 2', async () => {
+            const max = await this.deposits.maxDeposit(this.token2.address, carl);
+            assert.equal(500, max);
+        });
+
+        it('won\'t allow alice to deposit more token 2', async () => {
+            await this.token2.approve(this.deposits.address, 100, { from: alice });
+
+            try {
+                await this.deposits.deposit(this.token2.address, 100, {from: alice});
+            } catch (ex) {
+                assert.equal(ex.receipt.status, '0x0');
+                assert.equal(ex.reason, 'Not allowed to deposit specified amount of capped token');
+                return;
+            }
+            assert.fail('deposit successful');
+        });
+
+        it('won\'t allow bob to deposit more than max token 2', async () => {
+            await this.token2.approve(this.deposits.address, 250, { from: bob });
+
+            try {
+                await this.deposits.deposit(this.token2.address, 250, {from: bob});
+            } catch (ex) {
+                assert.equal(ex.receipt.status, '0x0');
+                assert.equal(ex.reason, 'Not allowed to deposit specified amount of capped token');
+                return;
+            }
+            assert.fail('deposit successful');
+        });
+
+        it('won\'t allow bob to deposit less than max token 2', async () => {
+            await this.token2.approve(this.deposits.address, 100, { from: bob });
+            await this.deposits.deposit(this.token2.address, 100, {from: bob});
+
+            const balanceToken2 = await this.token2.balanceOf(bob);
+            const depositToken2 = await this.deposits.deposited(this.token2.address, bob);
+            assert.equal(600, balanceToken2);
+            assert.equal(200, depositToken2);
+        });
+    });
+})
