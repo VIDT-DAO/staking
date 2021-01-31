1e1c9cf96dc464f27b208148d201ae69046246ac
Arnold Daniels <arnold@jasny.net>
2021-01-31 01:26:33 +0100

    Remove unnecessary imports.

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -4,7 +4,6 @@ pragma solidity 0.6.12;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
-import "@openzeppelin/contracts/utils/EnumerableSet.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./Deposits.sol";
